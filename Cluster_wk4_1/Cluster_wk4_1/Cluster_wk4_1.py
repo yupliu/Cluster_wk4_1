6b3c4c63480d00f60e4dd48e8e89bc226db69c3e
Alan Yu <yupliu@users.noreply.github.com>
2017-09-21 12:56:05 -0400

    Done

--- Cluster_wk4_1/Cluster_wk4_1/Cluster_wk4_1.py
+++ Cluster_wk4_1/Cluster_wk4_1/Cluster_wk4_1.py
@@ -143,9 +143,9 @@ def compute_covariances(data, resp, counts, means):
         weighted_sum = np.zeros((num_dim, num_dim))
         for i in range(num_data):
             # YOUR CODE HERE (Hint: Use np.outer on the data[i] and this cluster's mean)
-            weighted_sum += ...
+            weighted_sum += resp[i,k] * np.outer(data[i]-means[k],data[i]-means[k])
         # YOUR CODE HERE
-        covariances[k] = ...
+        covariances[k] = weighted_sum / counts[k]
 
     return covariances
 
@@ -162,3 +162,296 @@ if np.allclose(covariances[0], np.array([[0.60182827, 1.20365655], [1.20365655, 
     print 'Checkpoint passed!'
 else:
     print 'Check your code again.'
+
+def EM(data, init_means, init_covariances, init_weights, maxiter=1000, thresh=1e-4):
+    
+    # Make copies of initial parameters, which we will update during each iteration
+    means = init_means[:]
+    covariances = init_covariances[:]
+    weights = init_weights[:]
+    
+    # Infer dimensions of dataset and the number of clusters
+    num_data = len(data)
+    num_dim = len(data[0])
+    num_clusters = len(means)
+    
+    # Initialize some useful variables
+    resp = np.zeros((num_data, num_clusters))
+    ll = loglikelihood(data, weights, means, covariances)
+    ll_trace = [ll]
+    
+    for it in range(maxiter):
+        if it % 5 == 0:
+            print("Iteration %s" % it)
+        
+        # E-step: compute responsibilities
+        resp = compute_responsibilities(data, weights, means, covariances)
+
+        # M-step
+        # Compute the total responsibility assigned to each cluster, which will be useful when 
+        # implementing M-steps below. In the lectures this is called N^{soft}
+        counts = compute_soft_counts(resp)
+        
+        # Update the weight for cluster k using the M-step update rule for the cluster weight, \hat{\pi}_k.
+        # YOUR CODE HERE
+        weights = compute_weights(counts)
+        
+        # Update means for cluster k using the M-step update rule for the mean variables.
+        # This will assign the variable means[k] to be our estimate for \hat{\mu}_k.
+        # YOUR CODE HERE
+        means = compute_means(data,resp,counts)
+        
+        # Update covariances for cluster k using the M-step update rule for covariance variables.
+        # This will assign the variable covariances[k] to be the estimate for \hat{\Sigma}_k.
+        # YOUR CODE HERE
+        covariances = compute_covariances(data,resp,counts,means)
+        
+        # Compute the loglikelihood at this iteration
+        # YOUR CODE HERE
+        ll_latest = loglikelihood(data,weights,means,covariances)
+        ll_trace.append(ll_latest)
+        
+        # Check for convergence in log-likelihood and store
+        if (ll_latest - ll) < thresh and ll_latest > -np.inf:
+            break
+        ll = ll_latest
+    
+    if it % 5 != 0:
+        print("Iteration %s" % it)
+    
+    out = {'weights': weights, 'means': means, 'covs': covariances, 'loglik': ll_trace, 'resp': resp}
+
+    return out
+
+def generate_MoG_data(num_data, means, covariances, weights):
+    """ Creates a list of data points """
+    num_clusters = len(weights)
+    data = []
+    for i in range(num_data):
+        #  Use np.random.choice and weights to pick a cluster id greater than or equal to 0 and less than num_clusters.
+        k = np.random.choice(len(weights), 1, p=weights)[0]
+
+        # Use np.random.multivariate_normal to create data from this cluster
+        x = np.random.multivariate_normal(means[k], covariances[k])
+
+        data.append(x)
+    return data
+
+# Model parameters
+init_means = [
+    [5, 0], # mean of cluster 1
+    [1, 1], # mean of cluster 2
+    [0, 5]  # mean of cluster 3
+]
+init_covariances = [
+    [[.5, 0.], [0, .5]], # covariance of cluster 1
+    [[.92, .38], [.38, .91]], # covariance of cluster 2
+    [[.5, 0.], [0, .5]]  # covariance of cluster 3
+]
+init_weights = [1/4., 1/2., 1/4.]  # weights of each cluster
+
+# Generate data
+np.random.seed(4)
+data = generate_MoG_data(100, init_means, init_covariances, init_weights)
+
+plt.figure()
+d = np.vstack(data)
+plt.plot(d[:,0], d[:,1],'ko')
+plt.rcParams.update({'font.size':16})
+plt.tight_layout()
+plt.show()
+plt.close()
+
+np.random.seed(4)
+
+# Initialization of parameters
+chosen = np.random.choice(len(data), 3, replace=False)
+initial_means = [data[x] for x in chosen]
+initial_covs = [np.cov(data, rowvar=0)] * 3
+initial_weights = [1/3.] * 3
+
+# Run EM 
+results = EM(data, initial_means, initial_covs, initial_weights)
+
+import matplotlib.mlab as mlab
+def plot_contours(data, means, covs, title):
+    plt.figure()
+    plt.plot([x[0] for x in data], [y[1] for y in data],'ko') # data
+
+    delta = 0.025
+    k = len(means)
+    x = np.arange(-2.0, 7.0, delta)
+    y = np.arange(-2.0, 7.0, delta)
+    X, Y = np.meshgrid(x, y)
+    col = ['green', 'red', 'indigo']
+    for i in range(k):
+        mean = means[i]
+        cov = covs[i]
+        sigmax = np.sqrt(cov[0][0])
+        sigmay = np.sqrt(cov[1][1])
+        sigmaxy = cov[0][1]/(sigmax*sigmay)
+        Z = mlab.bivariate_normal(X, Y, sigmax, sigmay, mean[0], mean[1], sigmaxy)
+        plt.contour(X, Y, Z, colors = col[i])
+        plt.title(title)
+    plt.rcParams.update({'font.size':16})
+    plt.tight_layout()
+    plt.show()
+    plt.close()
+
+# Parameters after initialization
+plot_contours(data, initial_means, initial_covs, 'Initial clusters')
+
+# Parameters after 12 iterations
+results = EM(data, initial_means, initial_covs, initial_weights,maxiter=12) # YOUR CODE HERE
+plot_contours(data, results['means'], results['covs'], 'Clusters after 12 iterations')
+
+# Parameters after running EM to convergence
+results = EM(data, initial_means, initial_covs, initial_weights)
+plot_contours(data, results['means'], results['covs'], 'Final clusters')
+
+results = EM(data, initial_means, initial_covs, initial_weights)
+
+# YOUR CODE HERE
+loglikelihoods = results['loglik']
+
+plt.plot(range(len(loglikelihoods)), loglikelihoods, linewidth=4)
+plt.xlabel('Iteration')
+plt.ylabel('Log-likelihood')
+plt.rcParams.update({'font.size':16})
+plt.tight_layout()
+
+images = gl.SFrame('D:\\ML_Learning\\UW_Cluster\\week4\\images.sf\\')
+#gl.canvas.set_target('ipynb')
+import array
+images['rgb'] = images.pack_columns(['red', 'green', 'blue'])['X4']
+images.show()
+
+np.random.seed(1)
+
+# Initalize parameters
+init_means = [images['rgb'][x] for x in np.random.choice(len(images), 4, replace=False)]
+cov = np.diag([images['red'].var(), images['green'].var(), images['blue'].var()])
+init_covariances = [cov, cov, cov, cov]
+init_weights = [1/4., 1/4., 1/4., 1/4.]
+
+# Convert rgb data to numpy arrays
+img_data = [np.array(i) for i in images['rgb']]  
+
+# Run our EM algorithm on the image data using the above initializations. 
+# This should converge in about 125 iterations
+out = EM(img_data, init_means, init_covariances, init_weights)
+
+ll = out['loglik']
+plt.plot(range(len(ll)),ll,linewidth=4)
+plt.xlabel('Iteration')
+plt.ylabel('Log-likelihood')
+plt.rcParams.update({'font.size':16})
+plt.tight_layout()
+plt.show()
+plt.close()
+
+plt.figure()
+plt.plot(range(3,len(ll)),ll[3:],linewidth=4)
+plt.xlabel('Iteration')
+plt.ylabel('Log-likelihood')
+plt.rcParams.update({'font.size':16})
+plt.tight_layout()
+plt.show()
+plt.close()
+
+import colorsys
+def plot_responsibilities_in_RB(img, resp, title):
+    N, K = resp.shape
+    
+    HSV_tuples = [(x*1.0/K, 0.5, 0.9) for x in range(K)]
+    RGB_tuples = map(lambda x: colorsys.hsv_to_rgb(*x), HSV_tuples)
+    
+    R = img['red']
+    B = img['blue']
+    resp_by_img_int = [[resp[n][k] for k in range(K)] for n in range(N)]
+    cols = [tuple(np.dot(resp_by_img_int[n], np.array(RGB_tuples))) for n in range(N)]
+
+    plt.figure()
+    for n in range(len(R)):
+        plt.plot(R[n], B[n], 'o', c=cols[n])
+    plt.title(title)
+    plt.xlabel('R value')
+    plt.ylabel('B value')
+    plt.rcParams.update({'font.size':16})
+    plt.tight_layout()
+    plt.show()
+    plt.close()
+
+N, K = out['resp'].shape
+random_resp = np.random.dirichlet(np.ones(K), N)
+plot_responsibilities_in_RB(images, random_resp, 'Random responsibilities')
+
+out = EM(img_data, init_means, init_covariances, init_weights, maxiter=1)
+plot_responsibilities_in_RB(images, out['resp'], 'After 1 iteration')
+
+out = EM(img_data, init_means, init_covariances, init_weights, maxiter=20)
+plot_responsibilities_in_RB(images, out['resp'], 'After 20 iterations')
+
+
+#weights = out['weights']
+#means = out['means']
+#covariances = out['covs']
+#rgb = images['rgb']
+#N = 1
+#K = len(means) # number of clusters
+
+#assignments = [0]*N
+#probs = [0]*N
+
+## Compute the score of data point i under each Gaussian component:
+#p = np.zeros(K)
+#for k in range(K):
+#    p[k] = weights[k]*multivariate_normal.pdf(rgb[0], mean=means[k], cov=covariances[k])
+#    print p[k] 
+        
+## Compute assignments of each data point to a given cluster based on the above scores:
+#assignments[0] = np.argmax(p)
+#print assignments[0]    
+## For data point i, store the corresponding score under this cluster assignment:
+#probs[0] = np.max(p)
+#print probs[0]
+
+
+
+
+weights = out['weights']
+means = out['means']
+covariances = out['covs']
+rgb = images['rgb']
+N = len(images) # number of images
+K = len(means) # number of clusters
+
+assignments = [0]*N
+probs = [0]*N
+
+for i in range(N):
+    # Compute the score of data point i under each Gaussian component:
+    p = np.zeros(K)
+    for k in range(K):
+        p[k] = weights[k]*multivariate_normal.pdf(rgb[i], mean=means[k], cov=covariances[k])
+        
+    # Compute assignments of each data point to a given cluster based on the above scores:
+    assignments[i] = np.argmax(p)
+    
+    # For data point i, store the corresponding score under this cluster assignment:
+    probs[i] = np.max(p)
+
+assignments = gl.SFrame({'assignments':assignments, 'probs':probs, 'image': images['image']})
+
+def get_top_images(assignments, cluster, k=5):
+    # YOUR CODE HERE
+    images_in_cluster = assignments[assignments['assignments']==cluster]
+    top_images = images_in_cluster.topk('probs', k)
+    return top_images['image']
+
+#gl.canvas.set_target('ipynb')
+gl.canvas.set_target('browser')
+for component_id in range(4):
+    get_top_images(assignments, component_id).show()
+
+get_top_images(assignments, 0).show()
